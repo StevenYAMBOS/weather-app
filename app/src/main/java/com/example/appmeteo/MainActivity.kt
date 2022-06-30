5b2657606803fb0e3b9052de3c0e8a7e117e8a4e
Steven <stevenyambos@gmail.com>
2022-06-30 20:30:29 +0200

    Finition du design.

--- app/src/main/java/com/example/appmeteo/MainActivity.kt
+++ app/src/main/java/com/example/appmeteo/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.appmeteo
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val city : String = "Paris"
+    val API : String = "b067e82739b7721db838bf1d6b3ebb83"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>()
+    {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.main_container).visibility = View.GONE
+            findViewById<TextView>(R.id.error_text).visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+            TODO("Not yet implemented")
+        }
     }
 }
